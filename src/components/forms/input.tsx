f2edd5f1e86853e6187cafdf0ea1561e119dec91
samdevcoder45 <samdevcoder45@gmail.com>
2024-05-30 21:44:00 +0100

    completed project

--- src/components/forms/input.tsx
+++ src/components/forms/input.tsx
@@ -2,16 +2,28 @@ export default function Input({
   type = "text",
   placeholder,
   className,
+  value,
+  onBlur,
+  onFocus,
+  onChange,
   ...rest
 }: {
   type: string;
   placeholder: string;
   className: string;
+  value: string;
+  onBlur: () => void;
+  onFocus: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
   return (
     <input
       {...rest}
+      value={value}
       type={type}
+      onBlur={onBlur}
+      onFocus={onFocus}
+      onChange={onChange}
       placeholder={placeholder}
       className={className}
     />
